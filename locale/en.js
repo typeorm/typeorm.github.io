419543038739d049ba2edb25ffa07c67fb365a79
abingoal <abingoal@gmail.com>
2019-01-22 00:05:30 +0800

    add locale folder to store multi-language configs

---
+++ locale/en.js
@@ -0,0 +1,177 @@
+const locale_en = {
+    title: "TypeORM - Amazing ORM for TypeScript and JavaScript (ES7, ES6, ES5). Supports MySQL, PostgreSQL, MariaDB, SQLite, MS SQL Server, Oracle, WebSQL databases. Works in NodeJS, Browser, Ionic, Cordova and Electron platforms.",
+    contribute: "Found a typo and want to contribute to the documentation?",
+    edit: "Edit this page on Github!",
+    links: [{
+        url: "",
+        name: "Getting Started"
+    }, {
+        name: "Connection",
+        links: [{
+            url: "connection",
+            name: "Working with Connection"
+        }, {
+            url: "using-ormconfig",
+            name: "Using ormconfig.json"
+        }, {
+            url: "connection-options",
+            name: "Connection Options"
+        }, {
+            url: "multiple-connections",
+            name: "Multiple Connections"
+        }, {
+            url: "connection-api",
+            name: "Connection API"
+        }]
+    }, {
+        name: "Entity",
+        links: [{
+            url: "entities",
+            name: "What is Entity?"
+        }, {
+            url: "embedded-entities",
+            name: "Embedded Entities"
+        }, {
+            url: "entity-inheritance",
+            name: "Entity Inheritance"
+        }, {
+            url: "tree-entities",
+            name: "Tree Entities"
+        }, {
+            url: "separating-entity-definition",
+            name: "Separating Entity Definition"
+        }]
+    }, {
+        name: "Relations",
+        links: [{
+            url: "relations",
+            name: "What are Relations?"
+        }, {
+            url: "one-to-one-relations",
+            name: "One-to-One"
+        }, {
+            url: "many-to-one-one-to-many-relations",
+            name: "Many-to-One and One-to-Many"
+        }, {
+            url: "many-to-many-relations",
+            name: "Many-to-Many"
+        }, {
+            url: "eager-and-lazy-relations",
+            name: "Eager and Lazy Relations"
+        }, {
+            url: "relations-faq",
+            name: "Relations FAQ"
+        }]
+    }, {
+        name: "Entity Manager and Repository",
+        links: [{
+            url: "working-with-entity-manager",
+            name: "Working with Entity Manager"
+        }, {
+            url: "working-with-repository",
+            name: "Working with Repository"
+        }, {
+            url: "find-options",
+            name: "Find Options"
+        }, {
+            url: "custom-repository",
+            name: "Custom Repository"
+        }, {
+            url: "entity-manager-api",
+            name: "Entity Manager API"
+        }, {
+            url: "repository-api",
+            name: "Repository API"
+        }]
+    }, {
+        url: "query-builder",
+        name: "Query Builder",
+        links: [{
+            url: "select-query-builder",
+            name: "Select using Query Builder"
+        }, {
+            url: "insert-query-builder",
+            name: "Insert using Query Builder"
+        }, {
+            url: "update-query-builder",
+            name: "Update using Query Builder"
+        }, {
+            url: "delete-query-builder",
+            name: "Delete using Query Builder"
+        }, {
+            url: "relational-query-builder",
+            name: "Working with Relations"
+        }, {
+            url: "caching",
+            name: "Caching Results"
+        }]
+    }, {
+        name: "Advanced Topics",
+        links: [{
+            url: "migrations",
+            name: "Migrations"
+        }, {
+            url: "transactions",
+            name: "Transactions"
+        }, {
+            url: "indices",
+            name: "Indices"
+        }, {
+            url: "listeners-and-subscribers",
+            name: "Listeners and Subscribers"
+        }, {
+            url: "logging",
+            name: "Logging"
+        }, {
+            url: "using-cli",
+            name: "Using CLI"
+        }]
+    }, {
+        name: "Guides",
+        links: [{
+            url: "active-record-data-mapper",
+            name: "Active Record vs Data Mapper"
+        }, {
+            url: "mongodb",
+            name: "Working with MongoDB"
+        }, {
+            url: "validation",
+            name: "Using Validation"
+        }, {
+            url: "example-with-express",
+            name: "Example with Express"
+        }, {
+            url: "usage-with-javascript",
+            name: "Usage with JavaScript"
+        }, {
+            url: "sequelize-migration",
+            name: "Migration from Sequelize"
+        }]
+    }, {
+        name: "Help",
+        links: [{
+            url: "faq",
+            name: "FAQ"
+        }, {
+            url: "supported-platforms",
+            name: "Supported Platforms"
+        }, {
+            url: "decorator-reference",
+            name: "Decorator Reference"
+        },
+        // {
+        //     url: "internals",
+        //     name: "Internals"
+        // },
+        {
+            url: "roadmap",
+            name: "Roadmap"
+        }, {
+            url: "changelog",
+            name: "Changelog"
+        }, {
+            url: "support",
+            name: "Support"
+        }]
+    }]
+}
