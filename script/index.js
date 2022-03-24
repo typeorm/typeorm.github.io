ec15d517d2e8e352b6e1fb0e1d3f9bbcbfc97513
Gilad S <giladgd@gmail.com>
2022-03-24 23:36:52 +0200

    feat: change Vue router's mode from "hash" to "history" to support search engine indexing
    In order to support loading a deep url without getting a 404 error, a custom 404.html page is implemented to redirect back to index.html which uses history api to load the original url without a refresh.
    Based on #36

--- script/index.js
+++ script/index.js
@@ -11,14 +11,30 @@ const i18n = new VueI18n({
   messages // set locale messages
 })
 
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/:document?/:fragment?", component: DocumentPage },
+  ]
+});
+
+// Because the documentation previously used "hash" mode in the Router, paths
+// starting with a hash will be redirected to prevent breaking old permalinks.
+router.beforeEach((to, from, next) => {
+  if (to.fullPath.startsWith("/#/")) {
+    // Remove leading hash and replace following slash with hash.
+    // Before: https://typeorm.io/#/entities/entity-columns
+    // After:  https://typeorm.io/entities#entity-columns
+    const path = "/" + to.fullPath.substring("/#/".length).replace("/", "#");
+    next(path);
+  } else
+    next();
+});
+
 new Vue({
   el: "#app",
   i18n,
-  router: new VueRouter({
-    routes: [
-      { path: "/:document?/:fragment?", component: DocumentPage },
-    ]
-  }),
+  router: router,
   components: {
     "main-page": MainPage,
     "markdown-reader": MarkdownReader,
